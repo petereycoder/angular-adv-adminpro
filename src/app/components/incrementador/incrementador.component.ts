7c753b55353d059a1e332eace1864dec3c979d86
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-06 16:17:08 -0600

    @input - componente incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,11 +6,8 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class IncrementadorComponent {
-  progreso: number = 40;
-
-  get getPorcentaje(){
-    return `${this.progreso}%`;
-  }
+  //@Input('valor') progreso: number = 40; Se renombra el argumento
+  @Input() progreso: number = 40; //Puede recibir una propiedad desde el padre
 
   cambiarValor(valor: number){
     if (this.progreso + valor <= 100 && this.progreso + valor >=0 ) {
