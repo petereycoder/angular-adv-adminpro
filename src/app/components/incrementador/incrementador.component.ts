0cdfe107cbe0d5d1339427413ca80e2903695d3f
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-06 16:39:10 -0600

    @output - componente incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,12 +6,15 @@ import { Component, Input } from '@angular/core';
   styles: ``
 })
 export class IncrementadorComponent {
-  //@Input('valor') progreso: number = 40; Se renombra el argumento
-  @Input() progreso: number = 40; //Puede recibir una propiedad desde el padre
+  @Input('valor') progreso: number = 40; //Se renombra el argumento
+  //@Input() progreso: number = 40; Puede recibir una propiedad desde el padre
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();  //Como se fuera disparar un evento
 
   cambiarValor(valor: number){
     if (this.progreso + valor <= 100 && this.progreso + valor >=0 ) {
       this.progreso += valor;
+      this.valorSalida.emit(this.progreso);
     }
   }
 }
