6a3b2d8e72ddb04f65aa744c32da4d7a213c0712
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-09 11:54:07 -0600

    Se integró una libreria para las gráficas

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class Grafica1Component {
-
+// Doughnut
+public doughnutChartLabels: string[] = [
+  'Download Sales',
+  'In-Store Sales',
+  'Mail-Order Sales',
+];
+public doughnutChartData: ChartData<'doughnut'> = {
+  labels: this.doughnutChartLabels,
+  datasets: [
+    {
+      data: [350, 450, 100],
+      backgroundColor: ['#6857E6','#009FEE','#F02059']
+    },
+  ],
+};
+public doughnutChartType: ChartType = 'doughnut';
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { NgChartsModule } from 'ng2-charts';
+
 //Modulos
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
@@ -11,6 +13,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { FormsModule } from '@angular/forms';
+
 
 
 
@@ -32,7 +35,8 @@ import { FormsModule } from '@angular/forms';
     SharedModule,
     RouterModule,
     FormsModule,
-    ComponentsModule
+    ComponentsModule,
+    NgChartsModule,
   ]
 })
 export class PagesModule { }
