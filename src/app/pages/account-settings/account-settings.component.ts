ec1dc1c6031ae6adcb80ad43d28a8d9e5fcc8f13
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-11 10:11:07 -0600

    Se agregó clases de CSS mediante un método

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
   styles: ``
 })
-export class AccountSettingsComponent {
+export class AccountSettingsComponent implements OnInit {
 
+  public linkTheme = document.querySelector('#theme');
+  public links!: NodeListOf<Element>;
+
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
+
+  changeTheme(theme: string){
+
+    const url = `./assets/css/colors/${theme}.css`;
+
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme(){
+    this.links.forEach(elem => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if(btnThemeUrl === currentTheme){
+        elem.classList.add('working');
+      }
+    });
+  }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: ``
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
+
+  ngOnInit(): void {
+    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
+
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+  }
 
 }
