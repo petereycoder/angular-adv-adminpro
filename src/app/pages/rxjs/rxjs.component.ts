6c38cc6b62795d57e6138496a1ecab8ceb34fd95
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-12 15:38:11 -0600

    Funciones que retornen observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -8,35 +8,42 @@ import { Observable, retry } from 'rxjs';
 })
 export class RxjsComponent {
 
-  constructor(){
+  constructor() {
 
-    const obs$ = new Observable( observer => {
-      let i = -1;
 
-      const intervalo = setInterval( () => {
-        i++;
-        observer.next(i);
 
-        if( i === 4 ){
-          clearInterval(intervalo);
-          observer.complete();
-        }
-
-        if( i === 2 ){
-          i = 0;
-          observer.error('i llegó al valor de 2');
-        }
-
-      }, 1000)
-    });
-
-    obs$.pipe(
+    this.retornaObservable().pipe(
       retry(2)
     ).subscribe(
       valor => console.log('Subs', valor),
       error => console.warn('Error', error),
       () => console.info('Obs terminado')
     );
+
+
+  }
+
+  retornaObservable(): Observable<number> {
+    let i = -1;
+
+    return  new Observable<number>(observer => {
+
+      const intervalo = setInterval(() => {
+
+        i++;
+        observer.next(i);
+
+        if (i === 4) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          observer.error('i llegó al valor de 2');
+        }
+
+      }, 1000)
+    });
 
   }
 }
