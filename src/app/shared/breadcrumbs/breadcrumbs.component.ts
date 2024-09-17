cfdb32ef85ac5d548c380d9202f9f43e7ff3e30d
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-17 17:01:05 -0600

    Optimización de breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,31 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Event, Router, } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: ``
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   public titulo!: string;
+  public tituloSubs$!: Subscription;
 
   constructor( private router: Router){
-    this.getArgumentosRuta();
+    this.tituloSubs$ = this.getArgumentosRuta()
+                        .subscribe( ({ titulo }) => {
+                        this.titulo = titulo;
+                        document.title = `AdminPro - ${ titulo }`;
+                      });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
   getArgumentosRuta(){
 
-    this.router.events
+    return this.router.events
       .pipe(
         filter((event: Event): event is ActivationEnd => event instanceof ActivationEnd),
         filter((event: ActivationEnd) => event.snapshot.firstChild === null),
         map( ( event: ActivationEnd ) => event.snapshot.data ),
       )
-      .subscribe( ({ titulo }) => {
-        this.titulo = titulo;
-      });
 
   }
 }
